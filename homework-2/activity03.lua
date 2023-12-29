9f212752f7617661e2ffb198ac54639baff0179b
Abrantes Araújo Silva Fillho <abrantesasf@pm.me>
2023-12-29 12:01:36 -0300

    Finalização da activity03.

---
+++ homework-2/activity03.lua
@@ -0,0 +1,19 @@
+-- Week 2, Activity 3
+-- Stack machines
+
+-- a) Manually execute the following stack-machine program
+--    push 4
+--    push 2
+--    push 24
+--    push 21
+--    sub
+--    mult
+--    add
+
+--    4  ->  2  ->  24  ->  21  ->  3  ->  6  ->  10
+--           4       2      24      2      4
+--                   4       2      4
+--                           4
+
+-- b) What arithmetic expression would generate that previous program?
+--    (24 - 21) x 2 + 4
